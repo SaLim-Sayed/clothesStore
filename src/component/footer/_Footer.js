7d016fb9dab14cabaf21b5d79775e53b1628a5c5
Salim Sayed Salim Mohammed <75137018+SaLim-Sayed@users.noreply.github.com>
2023-06-21 13:02:48 +0300

    Update _Footer.js

--- src/component/footer/_Footer.js
+++ src/component/footer/_Footer.js
@@ -1,76 +1,153 @@
 import { Component } from "react";
-import {FaMapMarkerAlt} from 'react-icons/fa';
-import {FaPhone} from 'react-icons/fa';
-import {FaEnvelope} from 'react-icons/fa';
-import {FaFacebook} from 'react-icons/fa';
-import {FaInstagram} from 'react-icons/fa';
-import {FaLinkedin} from 'react-icons/fa';
-import {FaTwitter} from 'react-icons/fa';
+import { FaMapMarkerAlt } from "react-icons/fa";
+import { FaPhone } from "react-icons/fa";
+import { FaEnvelope } from "react-icons/fa";
+import { Nav } from 'react-bootstrap';
 
-class _Footer extends Component{
-    render(){
-        return(
-            
-            <div className="home" style={{background:'#05101c' , width:'100%' , height:'550px'}}>
-            
-            <div className="container">
-                <div className="row">
-
-
-                <div className="col-lg-4">
-                  <ul style={{listStyleType:'none' ,fontSize:'20px' ,lineHeight:'2.8' ,color:'rgb(193 193 193)',marginTop:'50px'}}>
-                  <li style={{color:'green' ,border:'none', marginBottom:'20px'}}><h3>SaLim Shop</h3></li>
-                <li> <a className="text-info" href="#"> <FaMapMarkerAlt/> 23 Salim street at minia</a></li>
-                <li> <a className="text-info" href="#"> <FaPhone/> 08684254254</a></li>
-                <li> <a className="text-info" href="#"><FaEnvelope/> salim@gmail.com</a></li>
-                 </ul>
+class _Footer extends Component {
+  render() {
+    return (
+      <div className="home" style={{ background: "#05101c", width: "100%",textAlign:"center" }}>
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-4">
+              <ul
+                style={{
+                  listStyleType: "none",
+                  fontSize: "20px",
+                  lineHeight: "2.8",
+                  color: "rgb(193 193 193)",
+                  marginTop: "50px",
+                }}
+              >
+                <li
+                  style={{
+                    color: "green",
+                    border: "none",
+                    marginBottom: "20px",
+                  }}
+                >
+                  <h3>SaLim Shop</h3>
+                </li>
+                <li>
+                  <Nav.Link className="text-info" to="#">
+                    <FaMapMarkerAlt /> 23 Salim street at minia
+                  </Nav.Link>
+                </li>
+                <li>
+                  <Nav.Link className="text-info" to="#">
+                    <FaPhone /> 08684254254
+                  </Nav.Link>
+                </li>
+                <li>
+                  <Nav.Link className="text-info" to="#">
+                    <FaEnvelope /> salim@gmail.com
+                  </Nav.Link>
+                </li>
+              </ul>
             </div>
 
-            
             <div className="col-lg-4">
-                  <ul style={{listStyleType:'none' ,fontSize:'20px' ,lineHeight:'2.2' ,color:'rgb(193 193 193)',marginTop:'50px'}}>
-                  <li style={{color:"green", border:'none', marginBottom:'20px'}}><h3>Products</h3></li>
-                
-                <li> <a className="text-info" href="#">Sport Wear</a></li>
-                <li> <a className="text-info" href="#">Men's Shoes</a></li>
-                <li> <a className="text-info" href="#">Women's Shoes</a></li>
-                <li> <a className="text-info" href="#">Popular Dress</a></li>
-                <li> <a className="text-info" href="#">Sport Shoes</a></li>
-                 </ul>
+              <ul
+                style={{
+                  listStyleType: "none",
+                  fontSize: "20px",
+                  lineHeight: "2.2",
+                  color: "rgb(193 193 193)",
+                  marginTop: "50px",
+                }}
+              >
+                <li
+                  style={{
+                    color: "green",
+                    border: "none",
+                    marginBottom: "20px",
+                  }}
+                >
+                  <h3>Products</h3>
+                </li>
+
+                <li>
+                  <Nav.Link className="text-info" to="#">
+                    Sport Wear
+                  </Nav.Link>
+                </li>
+                <li>
+                  <Nav.Link className="text-info" to="#">
+                    Men's Shoes
+                  </Nav.Link>
+                </li>
+                <li>
+                  <Nav.Link className="text-info" to="#">
+                    Women's Shoes
+                  </Nav.Link>
+                </li>
+                <li>
+                  <Nav.Link className="text-info" to="#">
+                    Popular Dress
+                  </Nav.Link>
+                </li>
+                <li>
+                  <Nav.Link className="text-info" to="#">
+                    Sport Shoes
+                  </Nav.Link>
+                </li>
+              </ul>
             </div>
 
             <div className="col-lg-4 ">
-                  <ul style={{listStyleType:'none' ,fontSize:'20px' ,lineHeight:'2.2' ,color:'rgb(193 193 193)',marginTop:'50px'}}>
-                  <li style={{color:"green",border:'none', marginBottom:'20px'}}><h3>Further Info</h3></li>
-                <li> <a className="text-info" href="#">Home</a></li>
-                <li> <a className="text-info" href="#">About Us</a></li>
-                <li> <a className="text-info" href="#">Shop Locations</a></li>
-                <li> <a className="text-info" href="#">FAQS</a></li>
-                <li> <a className="text-info" href="#">Contact</a></li>
-                 </ul>
-            
+              <ul
+                style={{
+                  listStyleType: "none",
+                  fontSize: "20px",
+                  lineHeight: "2.2",
+                  color: "rgb(193 193 193)",
+                  marginTop: "50px",
+                }}
+              >
+                <li
+                  style={{
+                    color: "green",
+                    border: "none",
+                    marginBottom: "20px",
+                  }}
+                >
+                  <h3>Further Info</h3>
+                </li>
+                <li>
+                  <Nav.Link className="text-info" to="#">
+                    Home
+                  </Nav.Link>
+                </li>
+                <li>
+                  <Nav.Link className="text-info" to="#">
+                    About Us
+                  </Nav.Link>
+                </li>
+                <li>
+                  <Nav.Link className="text-info" to="#">
+                    Shop Locations
+                  </Nav.Link>
+                </li>
+                <li>
+                  <Nav.Link className="text-info" to="#">
+                    FAQS
+                  </Nav.Link>
+                </li>
+                <li>
+                  <Nav.Link className="text-info" to="#">
+                    Contact
+                  </Nav.Link>
+                </li>
+              </ul>
             </div>
-            <hr/>
-            
-            
-            <div className="icons p-5"  style={{color:'white'}}>
-            <hr/>
-                <a href="#"> <FaFacebook style={{marginRight:'20px'}}/></a>
-                <a href="#"><FaInstagram  style={{marginRight:'20px'}}/></a>
-                <a href="#"><FaTwitter  style={{marginRight:'20px'}}/></a>
-                <a href="#"><FaLinkedin  style={{marginRight:'20px'}}/></a>
-            </div>
+            <hr />
 
-            <div className="sub p-5">
-            <hr/>
-                <input type="text" style={{background:'black' , border :'1px solid white' ,float:'right',position:'absolute' , left:'670px' , width:'250px' , height:'40px'}} className="text-light" placeholder="Email Address"></input>
-                <button className="btn btn-success mx-4"  style={{width:'100px' , height:'40px',float:'right',position:'absolute' , left:'900px'}}>Subscribe</button>
-            </div>
-
-            </div>
-            </div>
-            </div>  
-        );
-    }
+           
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 export default _Footer;
